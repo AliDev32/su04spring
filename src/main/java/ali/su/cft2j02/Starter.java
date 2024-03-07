0a32a4567cc8be8dc51cd64116683452ba1fcc73
Торков Алексей Михайлович <torkov@dev.vtb.ru>
2024-03-07 13:46:08 +0300

    Starter on interfaces

--- src/main/java/ali/su/cft2j02/Starter.java
+++ src/main/java/ali/su/cft2j02/Starter.java
@@ -2,12 +2,14 @@ package ali.su.cft2j02;
 
 import ali.su.cft2j02.config.ReaderConfig;
 import ali.su.cft2j02.datareader.DataFileReader;
+import ali.su.cft2j02.datareader.DataReader;
 import ali.su.cft2j02.datasaver.*;
 import ali.su.cft2j02.middleworks.MiddleWorksRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = "ali.su.cft2j02")
 public class Starter {
@@ -15,14 +17,14 @@ public class Starter {
         var ctx = SpringApplication.run(Starter.class);
 
         //чтение данных
-        var data = ctx.getBean(DataFileReader.class).get();
+        var data = (List<Data>) ctx.getBean(DataReader.class).get();
 
         //промежуточные обработки
         var workRunner = ctx.getBean(MiddleWorksRunner.class);
         data = workRunner.doWork(data);
 
         //сохранение данных
-        var saver = ctx.getBean(DataDbSaver.class);
+        var saver = ctx.getBean(DataSaver.class);
         saver.accept(data);
     }
 }
